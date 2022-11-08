384696487bb293af21ba971caa36f528380e23a2
lozin <lozindenis6@gmail.com>
2022-11-08 20:01:43 +0200

    Create work "2"

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+import random
 def two():
     try:
+        size = int(input('size - '))
+        begin = int(input('begin - '))
+        end = int(input('end - '))
+        my_list = list()
 
+        for i in range(size):
+            my_list.append(random.randint(begin, end))
+        print(f'{my_list}')
 
-
+        odd = []
+        even = []
+        neg = []
+        pos = []
 
 
 
