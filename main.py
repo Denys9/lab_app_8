0bff0bea7a8795fb9cae27f7a3ae3e859412aa1e
lozin <lozindenis6@gmail.com>
2022-11-08 19:26:52 +0200

    Create work "1"

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ def one():
         neg = 0
         odd = []
         even = []
+        mul_3 = 1
+        start_1 = []
+        end_1 = []
+
         for i in range(size):
             my_list.append(random.randint(begin,end))
         print(f'{my_list}')
+
         for i in my_list:
             if i < 0:
                 neg += i
@@ -18,7 +23,15 @@ def one():
                 even.append(i)
             if i%2 !=0:
                 odd.append(i)
+        for i in range(3, len(my_list), 3):
+            mul_3 *= my_list[i]
+
+
         print(f'sum neg = {neg}\nsum odd = {sum(odd)}\nsum even = {sum(even)}')
+        print(f'mul even 3 index = {mul_3}')
+        print(f'')
+
+
     except Exception as ex:
             print(f'Eror information: {ex}')
 
